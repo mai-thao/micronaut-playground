73146f040e1a21b35cbb51618b6b7db2da6c1cb8
mai-thao <mthaominn@gmail.com>
2025-07-19 22:47:22 -0500

    Add Scheduled annotation with cron expression

--- purge-scheduler/src/main/kotlin/com/example/PurgeScheduler.kt
+++ purge-scheduler/src/main/kotlin/com/example/PurgeScheduler.kt
@@ -1,10 +1,23 @@
 package com.example
 
+import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import java.time.LocalDateTime
 
 @Singleton
 class PurgeScheduler(private val repo: UserRepository) {
+
+    /**
+     * * * * * * *
+     * | | | | | |
+     * | | | | | +-- Year (optional)
+     * | | | | +---- Day of the week (0 - 7, where both 0 and 7 represent Sunday)
+     * | | | +------ Month (1 - 12)
+     * | | +-------- Day of the month (1 - 31)
+     * | +---------- Hour (0 - 23)
+     * +------------ Minute (0 - 59)
+     */
+    @Scheduled(cron = "*/10 * * * * *") // This cron expression invokes the method every 10 second
     fun purgeData() {
         println("purgeData() called")
         val cutoff = LocalDateTime.now().minusDays(30) // Delete any records older than 30 days
