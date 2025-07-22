18ebc9e12b7718ace2dc9970ae1694ed3ffc8680
mai-thao <mthaominn@gmail.com>
2025-07-21 23:43:57 -0500

    Remove magical purge day and inject from config file instead

--- purge-scheduler/src/main/kotlin/com/example/PurgeScheduler.kt
+++ purge-scheduler/src/main/kotlin/com/example/PurgeScheduler.kt
@@ -1,11 +1,15 @@
 package com.example
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import java.time.LocalDateTime
 
 @Singleton // Create only one instance of this class, to prevent duplicate scheduled job runs and ensures thread safety
-class PurgeScheduler(private val repo: UserRepository) {
+class PurgeScheduler(
+    private val repo: UserRepository,
+    @Value("\${database.purge.days}") private val ttlDays: Long
+) {
 
     /**
      * * * * * * *
@@ -20,8 +24,8 @@ class PurgeScheduler(private val repo: UserRepository) {
     @Scheduled(cron = "*/10 * * * * *") // This cron expression invokes the method every 10 second
     fun purgeData() {
         println("purgeData() called")
-        val cutoff = LocalDateTime.now().minusDays(30) // Delete any records older than 30 days
+        val cutoff = LocalDateTime.now().minusDays(ttlDays) // Delete any records older than 30 days
         val deletedCount = repo.deleteByCreatedTimestampBefore(cutoff)
-        println(deletedCount)
+        println("Deleted $deletedCount records older than $ttlDays days ago")
     }
 }
