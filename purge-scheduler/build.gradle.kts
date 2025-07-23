08ac46afbff6a80d4d4e615ec53356dc95a8c0a3
mai-thao <mthaominn@gmail.com>
2025-07-22 22:45:46 -0500

    Add init unit test

--- purge-scheduler/build.gradle.kts
+++ purge-scheduler/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("io.mockk:mockk:1.14.5")
 }
 
 application {
--- purge-scheduler/src/test/kotlin/com/example/PurgeSchedulerTest.kt
+++ purge-scheduler/src/test/kotlin/com/example/PurgeSchedulerTest.kt
@@ -1,19 +1,24 @@
 package com.example
 
-import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
-import jakarta.inject.Inject
 
 @MicronautTest
 class PurgeSchedulerTest {
 
-    @Inject
-    lateinit var application: EmbeddedApplication<*>
+    private val repo = mockk<UserRepository>()
+    private val purgeDays = 1L
+    private val purgeScheduler = PurgeScheduler(repo, purgeDays)
 
     @Test
-    fun testItWorks() {
-        Assertions.assertTrue(application.isRunning)
+    fun `should call deleteByCreatedTimestampBefore`() {
+        every { repo.deleteByCreatedTimestampBefore(any()) } returns 1
+
+        purgeScheduler.purgeData()
+
+        verify { repo.deleteByCreatedTimestampBefore(any()) }
     }
 }
